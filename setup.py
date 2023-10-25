82f7cdd5ef9eecc0c3e39b6c769126473809eeff
Alden Swain <aldenswain1@gmail.com>
2023-10-24 20:12:42 -0400

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="inclass_mod9"
+    version="0.1"
+    description= "it'll scare ya"
+    url= "https://github.com/aswain4/inclass_mod9"
+    author= "Alden Swain"
+    author_email= "yhp7nq@virginia.edu"
+    license="MIT"
+    packages=['demo']
+)
